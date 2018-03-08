ba7ee7638e82666665952b0dc104b8bd4bfbcdf4
Marcio Garcia <marcio.garcia@rga.com>
2018-03-08 12:39:36 -0300

    website and set policy

--- src/s3bucket/src/cmd/create/create.go
+++ src/s3bucket/src/cmd/create/create.go
@@ -12,7 +12,7 @@ import (
 func CreateBucket() {
 
 	if hasBucketName() {
-		fmt.Println("Creating Bucket: ", defaults.BucketName, "  Region: ", defaults.Region)
+		fmt.Println("Creating Bucket:", defaults.BucketName, " Region:", defaults.Region)
 		create()
 
 	} else {
@@ -21,11 +21,13 @@ func CreateBucket() {
 	}
 }
 
+//
+//
 func create() {
 	if bucketAlreadyCreated() {
 		fmt.Println("Bucket already exits")
 	} else {
-		fmt.Println("Creating...")
+		fmt.Println(" - creating bucket")
 		var sess = myaws.GetAwsSession()
 		var svc = s3.New(sess)
 
@@ -33,16 +35,58 @@ func create() {
 			Bucket: aws.String(defaults.BucketName),
 		})
 		if err != nil {
-			fmt.Println("Unable to create bucket %q, %v", defaults.BucketName, err)
+			fmt.Println("Unable to create bucket", defaults.BucketName, err)
+		} else {
+			setWebsite()
 		}
+	}
 
-		// // Wait until bucket is created before finishing
-		// fmt.Printf("Waiting for bucket %q to be created...\n", bucket)
+}
 
-		// err = svc.WaitUntilBucketExists(&s3.HeadBucketInput{
-		// 	Bucket: aws.String(bucket),
-		// })
+//
+//
+func setWebsite() {
+	fmt.Println(" - seting as static website")
+	var sess = myaws.GetAwsSession()
+	var svc = s3.New(sess)
 
+	params := s3.PutBucketWebsiteInput{
+		Bucket: aws.String(defaults.BucketName),
+		WebsiteConfiguration: &s3.WebsiteConfiguration{
+			IndexDocument: &s3.IndexDocument{
+				Suffix: aws.String("index.html"),
+			},
+			ErrorDocument: &s3.ErrorDocument{
+				Key: aws.String("error.html"),
+			},
+		},
+	}
+
+	_, err := svc.PutBucketWebsite(&params)
+	if err != nil {
+		fmt.Println("Unable to set bucket %q website configuration:", defaults.BucketName, err)
+	} else {
+		setBucketPolicy()
+	}
+
+}
+
+//
+//
+func setBucketPolicy() {
+	fmt.Println(" - applying bucket policies")
+
+	policy := "{\"Version\": \"2012-10-17\", \"Statement\": [{\"Sid\": \"AddPerm\",\"Effect\": \"Allow\",\"Principal\": {\"AWS\": \"*\"},\"Action\": \"s3:GetObject\",\"Resource\": \"arn:aws:s3:::" + defaults.BucketName + "/*\"}]}"
+
+	var sess = myaws.GetAwsSession()
+	var svc = s3.New(sess)
+
+	_, err := svc.PutBucketPolicy(&s3.PutBucketPolicyInput{
+		Bucket: aws.String(defaults.BucketName),
+		Policy: aws.String(string(policy)),
+	})
+	if err != nil {
+		fmt.Println("Unable to set bucket policy:", defaults.BucketName, err)
 	}
 
 }
