e06057bca2d769e4a8521b296cc17c2abc143dc1
Marcio Garcia <marcio.garcia@rga.com>
2018-05-10 12:18:49 -0300

    done

--- src/gmail/README.md
+++ src/gmail/README.md
@@ -28,7 +28,7 @@ __Sending an email__
 
 ... to create a bucket, go into ```gmail``` after the __Development__ steps and run:
 
-    go run main.go send TO_EMAIL, SUBJECT, CONTENT
+    go run main.go send TO_EMAIL, SUBJECT, HTML_FILE
 
 
 ## Generating 
@@ -37,3 +37,27 @@ __Sending an email__
     mv main gmail
 
 
+## HTML File Example
+
+
+```
+<!DOCTYPE HTML PULBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html"; charset="ISO-8859-1">
+</head>
+<body>
+<b>Howdy!</b><br>
+&nbsp;<br>
+Eu sou o robo do pessoal de tecnologia da R/GA.<br>
+Este email é para te avisar que o ambiente: <a href="https://rga.com">https://rga.com</a> 
+está atualizado e você já pode dar uma olhada em como está ficando o progresso. <br>
+<div class="moz-signature"><i><br>
+<br>
+Regards<br>
+<br>
+R/GA SP Tech 
+<i></div>
+</body>
+</html>
+```
--- src/gmail/cmd/send/send.go
+++ src/gmail/cmd/send/send.go
@@ -1,5 +1,117 @@
 package send
 
-func Send() {
+import (
+	"bytes"
+	"fmt"
+	"gmail/cmd/defaults"
+	"io/ioutil"
+	"mime/quotedprintable"
+	"net/smtp"
+	"strings"
+)
 
+/**
+	Modified from https://gist.github.com/jpillora/cb46d183eca0710d909a
+	Thank you very much.
+**/
+
+const (
+	SMTPServer = "smtp.gmail.com"
+)
+
+type Sender struct {
+	User     string
+	Password string
+}
+
+func Send() {
+	sender := Sender{defaults.Email, defaults.Pwd}
+
+	Receiver := strings.Split(defaults.To, ",")
+	Subject := defaults.Subject
+	message := getEmailContent()
+
+	// bodyMessage := sender.writeHTMLEmail(Receiver, Subject, message)
+	// sender.sendMail(Receiver, Subject, bodyMessage)
+
+	for i := range strings.Split(defaults.To, ",") {
+		bodyMessage := sender.writeHTMLEmail(Receiver[i], Subject, message)
+		sender.sendMail(Receiver[i], Subject, bodyMessage)
+	}
+
+}
+
+func (sender Sender) sendMail(Dest string, Subject, bodyMessage string) {
+
+	// msg := "From: " + sender.User + "\n" +
+	// 	"To: " + strings.Join(Dest, ",") + "\n" +
+	// 	"Subject: " + Subject + "\n" + bodyMessage
+
+	msg := "From: " + sender.User + "\n" +
+		"To: " + Dest + "\n" +
+		"Subject: " + Subject + "\n" + bodyMessage
+
+	err := smtp.SendMail(SMTPServer+":587",
+		smtp.PlainAuth("", sender.User, sender.Password, SMTPServer), sender.User, []string{Dest}, []byte(msg))
+
+	if err != nil {
+		fmt.Printf("Ups.... SMTP error: %s", err)
+		return
+	}
+
+	fmt.Println("Cheers! Mail sent successfully!")
+}
+
+func (sender Sender) writeEmail(dest string, contentType, subject, bodyMessage string) string {
+
+	header := make(map[string]string)
+	header["From"] = sender.User
+
+	// receipient := dest
+	// receipient := ""
+
+	// for _, user := range dest {
+	// 	receipient = receipient + user
+	// }
+
+	// header["To"] = receipient
+	header["Subject"] = subject
+	header["MIME-Version"] = "1.0"
+	header["Content-Type"] = fmt.Sprintf("%s; charset=\"utf-8\"", contentType)
+	header["Content-Transfer-Encoding"] = "quoted-printable"
+	header["Content-Disposition"] = "inline"
+
+	message := ""
+
+	for key, value := range header {
+		message += fmt.Sprintf("%s: %s\r\n", key, value)
+	}
+
+	var encodedMessage bytes.Buffer
+
+	finalMessage := quotedprintable.NewWriter(&encodedMessage)
+	finalMessage.Write([]byte(bodyMessage))
+	finalMessage.Close()
+
+	message += "\r\n" + encodedMessage.String()
+
+	return message
+}
+
+func (sender *Sender) writeHTMLEmail(dest string, subject, bodyMessage string) string {
+	return sender.writeEmail(dest, "text/html", subject, bodyMessage)
+}
+
+func (sender *Sender) writePlainEmail(dest string, subject, bodyMessage string) string {
+	return sender.writeEmail(dest, "text/plain", subject, bodyMessage)
+}
+
+func getEmailContent() string {
+	b, err := ioutil.ReadFile(defaults.Content)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	str := string(b) // convert content to a 'string'
+	return str
 }
